a8e893367f16f6a68735fcf6ab0f2f382ba0aee3
Fernando Tomio <nandotomio@gmail.com>
2021-01-26 16:48:11 -0300

    feat: add env var to API URL

--- src/main/factories/http/api-url-factory.ts
+++ src/main/factories/http/api-url-factory.ts
@@ -1,1 +1,1 @@
-export const makeApiUrl = (path: string): string => `https://clean-node-api-project.herokuapp.com/api${path}`
+export const makeApiUrl = (path: string): string => `process.env.API_URL${path}`
