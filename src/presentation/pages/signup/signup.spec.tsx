141a5579cf787cbdf59a7f55de5faec76aafecea
Fernando Tomio <nandotomio@gmail.com>
2021-01-28 18:49:46 -0300

    test: ensure SignUp redirects to Login on link click

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -184,4 +184,12 @@ describe('SignUp Component', () => {
     FormHelper.testElementText(sut, 'main-error', error.message)
     FormHelper.testChildCount(sut, 'error-wrap', 1)
   })
+
+  test('should go to login page', async () => {
+    const { sut } = makeSut()
+    const loginLink = sut.getByTestId('login-link')
+    fireEvent.click(loginLink)
+    expect(history.length).toBe(1)
+    expect(history.location.pathname).toBe('/login')
+  })
 })
