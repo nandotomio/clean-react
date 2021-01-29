5a89e720dd1a65224ff8e2498fa4350cb264786e
Fernando Tomio <nandotomio@gmail.com>
2021-01-28 23:16:55 -0300

    test: ensure login begin with correct initial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,15 @@
 describe('Login', () => {
-  it('should load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('login')
+  })
+  it('should load with correct initial state', () => {
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Required field.')
+      .should('contain.text', '🔴')
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Required field.')
+      .should('contain.text', '🔴')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 })
