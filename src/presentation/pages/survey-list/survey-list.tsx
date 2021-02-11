54ce508c04dbb13a9d29227fa62e10b3bc36df0a
Fernando Tomio <nandotomio@gmail.com>
2021-02-11 10:43:00 -0300

    refactor: adapt layout to mobile

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -21,6 +21,30 @@ const SurveyList: React.FC = () => {
             </div>
             <footer>Check Result</footer>
           </li>
+          <li>
+            <div className={Styles.surveyContent}>
+              <Icon className={Styles.iconWrap} iconName={IconName.thumbUp} />
+              <time>
+                <span className={Styles.day}>22</span>
+                <span className={Styles.month}>03</span>
+                <span className={Styles.year}>2021</span>
+              </time>
+              <p>What is your favorite web framework?</p>
+            </div>
+            <footer>Check Result</footer>
+          </li>
+          <li>
+            <div className={Styles.surveyContent}>
+              <Icon className={Styles.iconWrap} iconName={IconName.thumbUp} />
+              <time>
+                <span className={Styles.day}>22</span>
+                <span className={Styles.month}>03</span>
+                <span className={Styles.year}>2021</span>
+              </time>
+              <p>What is your favorite web framework?</p>
+            </div>
+            <footer>Check Result</footer>
+          </li>
         </ul>
       </div>
       <Footer />
