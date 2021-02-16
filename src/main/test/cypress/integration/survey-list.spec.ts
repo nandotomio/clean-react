047f60770a53aa8d274f2ca4099ddef3942ea889
Fernando Tomio <nandotomio@gmail.com>
2021-02-16 11:23:35 -0300

    test: ensure SurveyList logout on logout link click

--- src/main/test/cypress/integration/survey-list.spec.ts
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -28,4 +28,11 @@ describe('SurveyList', () => {
     const { name } = Helper.getLocalStorageItem('account')
     cy.getByTestId('username').should('contain.text', name)
   })
+
+  it('should logout on logout link click', () => {
+    Http.mockUnexpectedError()
+    cy.visit('')
+    cy.getByTestId('logout').click()
+    Helper.testUrl('/login')
+  })
 })
