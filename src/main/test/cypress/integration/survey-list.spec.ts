fd092c21f2343c017cd0eaa6fb73ae3f1048828b
Fernando Tomio <nandotomio@gmail.com>
2021-02-16 11:16:26 -0300

    test: ensure SurveyList present error on UnexpectedError

---
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -0,0 +1,18 @@
+import * as Helper from '../support/helpers'
+import * as Http from '../support/survey-list-mocks'
+import faker from 'faker'
+
+describe('SurveyList', () => {
+  beforeEach(() => {
+    Helper.setLocalStorageItem('account', {
+      accessToken: faker.random.uuid(),
+      name: faker.name.findName()
+    })
+  })
+
+  it('should present error on UnexpectedError', () => {
+    Http.mockUnexpectedError()
+    cy.visit('')
+    cy.getByTestId('error').should('contain.text', 'Oops. Something went wrong. Please try again later.')
+  })
+})
