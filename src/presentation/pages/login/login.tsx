22c126ddfc62771f6638302a1732c0ceafca4e9f
Fernando Tomio <nandotomio@gmail.com>
2021-02-17 15:51:43 -0300

    refactor: change setState to use callback

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -48,7 +48,7 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
       setCurrentAccount(account)
       history.replace('/')
     } catch (error) {
-      setState({ ...state, isLoading: false, mainError: error.message })
+      setState(old => ({ ...old, isLoading: false, mainError: error.message }))
     }
   }
 
--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -56,7 +56,7 @@ const SignUp: React.FC<Props> = ({ validation, addAccount }: Props) => {
       setCurrentAccount(account)
       history.replace('/')
     } catch (error) {
-      setState({ ...state, isLoading: false, mainError: error.message })
+      setState(old => ({ ...old, isLoading: false, mainError: error.message }))
     }
   }
 
