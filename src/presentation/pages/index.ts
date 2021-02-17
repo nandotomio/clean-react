073c4ebed68c14aa8582619423818339aae683d4
Fernando Tomio <nandotomio@gmail.com>
2021-02-17 14:51:50 -0300

    feat: add SurveyResult layout

--- src/presentation/pages/index.ts
+++ src/presentation/pages/index.ts
@@ -1,3 +1,4 @@
 export { default as Login } from '@/presentation/pages/login/login'
 export { default as SignUp } from '@/presentation/pages/signup/signup'
 export { default as SurveyList } from '@/presentation/pages/survey-list/survey-list'
+export { default as SurveyResult } from '@/presentation/pages/survey-result/survey-result'
---
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -0,0 +1,42 @@
+import Styles from './survey-result-styles.scss'
+import { Footer, Header, Spinner } from '@/presentation/components'
+import FlipMove from 'react-flip-move'
+import React from 'react'
+
+const SurveyResult: React.FC = () => {
+  return (
+    <div className={Styles.surveyResultWrap}>
+      <Header />
+      <div className={Styles.contentWrap}>
+        <h2>What is your favorite web framework?</h2>
+        <FlipMove className={Styles.answersList}>
+          <li>
+            <img src="https://clean-node-api-project.herokuapp.com/static/img/logo-react.png" alt="answer image"/>
+            <span className={Styles.answer}>ReactJs</span>
+            <span className={Styles.percent}>50%</span>
+          </li>
+          <li className={Styles.active}>
+            <img src="https://clean-node-api-project.herokuapp.com/static/img/logo-react.png" alt="answer image"/>
+            <span className={Styles.answer}>ReactJs</span>
+            <span className={Styles.percent}>50%</span>
+          </li>
+          <li>
+            <img src="https://clean-node-api-project.herokuapp.com/static/img/logo-react.png" alt="answer image"/>
+            <span className={Styles.answer}>ReactJs</span>
+            <span className={Styles.percent}>50%</span>
+          </li>
+        </FlipMove>
+        <button>Go Back</button>
+        <div className={Styles.loadingWrap}>
+          <div className={Styles.loading}>
+            <span>Loading, please wait...</span>
+            <Spinner isNegative />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default SurveyResult
