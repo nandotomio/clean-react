14fc1c46d44288a38c17ca42e0f796732f3bd473
Fernando Tomio <nandotomio@gmail.com>
2021-02-18 17:17:27 -0300

    refactor: update packages

--- src/main/factories/pages/survey-result/survey-result-factory.tsx
+++ src/main/factories/pages/survey-result/survey-result-factory.tsx
@@ -3,8 +3,12 @@ import { SurveyResult } from '@/presentation/pages'
 import { useParams } from 'react-router-dom'
 import React from 'react'
 
+type UseParamsProps = {
+  id: string
+}
+
 export const makeSurveyResult: React.FC = () => {
-  const { id } = useParams<any>()
+  const { id } = useParams<UseParamsProps>()
 
   return (
     <SurveyResult
