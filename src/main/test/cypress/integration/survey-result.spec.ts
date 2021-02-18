f1bd0f64c45a29d2f406172156118bc0abbfa8f8
Fernando Tomio <nandotomio@gmail.com>
2021-02-18 16:01:28 -0300

    test: ensure SurveyResult logout on AccessDeniedError

--- src/main/test/cypress/integration/survey-result.spec.ts
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -66,6 +66,7 @@ describe('SurveyResult', () => {
 
   describe('save', () => {
     const mockUnexpectedError = (): void => Http.mockServerError(path, 'PUT')
+    const mockAccessDeniedError = (): void => Http.mockForbiddenError(path, 'PUT')
 
     beforeEach(() => {
       cy.fixture('account').then(account => {
@@ -79,6 +80,12 @@ describe('SurveyResult', () => {
       mockUnexpectedError()
       cy.get('li:nth-child(2)').click()
       cy.getByTestId('error').should('contain.text', 'Oops. Something went wrong. Please try again later.')
+    })
+
+    it('should present error on AccessDeniedError', () => {
+      mockAccessDeniedError()
+      cy.get('li:nth-child(2)').click()
+      Helper.testUrl('/login')
     })
   })
 })
