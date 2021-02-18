2984dd22a2058b721a9a89ccafce031ee6b0fce1
Fernando Tomio <nandotomio@gmail.com>
2021-02-18 18:19:13 -0300

    refactor: change SurveyList to use recoil

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,24 +1,22 @@
 import Styles from './survey-list-styles.scss'
-import { Footer, Header, Error } from '@/presentation/components'
-import { SurveyListItem } from '@/presentation/pages/survey-list/components'
+import { Header, Footer, Error } from '@/presentation/components'
+import { SurveyListItem, surveyListState } from '@/presentation/pages/survey-list/components'
 import { useErrorHandler } from '@/presentation/hooks'
 import { LoadSurveyList } from '@/domain/usecases'
-import React, { useEffect, useState } from 'react'
+
+import { useRecoilState } from 'recoil'
+import React, { useEffect } from 'react'
 
 type Props = {
   loadSurveyList: LoadSurveyList
 }
 
 const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
-  const [state, setState] = useState({
-    surveys: [] as LoadSurveyList.Model[],
-    error: '',
-    reload: false
-  })
   const handleError = useErrorHandler((error: Error) => {
     setState(old => ({ ...old, error: error.message }))
   })
-  const reload = (): void => setState(old => ({ ...old, surveys: [], error: '', reload: !old.reload }))
+  const [state, setState] = useRecoilState(surveyListState)
+  const reload = (): void => setState(old => ({ surveys: [], error: '', reload: !old.reload }))
 
   useEffect(() => {
     loadSurveyList.loadAll()
@@ -30,8 +28,11 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
     <div className={Styles.surveyListWrap}>
       <Header />
       <div className={Styles.contentWrap}>
-        <h2>Surveys</h2>
-          {state.error ? <Error error={state.error} reload={reload} /> : <SurveyListItem surveys={state.surveys} />}
+        <h2>Enquetes</h2>
+        {state.error
+          ? <Error error={state.error} reload={reload} />
+          : <SurveyListItem surveys={state.surveys} />
+        }
       </div>
       <Footer />
     </div>
